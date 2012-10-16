3e7865cfa89e6766f8f47bbf8c429e7b8892e6d4
Konrad Kurdej <konrad.kurdej@10clouds.com>
2012-10-16 15:33:07 +0200

    Added perf testing

---
+++ perf_data/test_on_data.py
@@ -0,0 +1,72 @@
+import sys
+import csv
+import pprint
+import time
+
+sys.path.append('../')
+from troia_client import TroiaClient
+
+
+def load_costs(path):
+    cost = csv.reader(open(path + "/costs"), delimiter='\t')
+    dictt = {}
+    for c1, c2, cost_ in cost:
+        el = dictt.get(c1, {})
+        el[c2] = cost_
+        dictt[c1] = el
+    return [{'name':c, 'prior':1., 'misclassification_cost':d}
+                                                for c, d in dictt.iteritems()]
+
+
+def load_correct(path):
+    correct = csv.reader(open(path + "/correct"), delimiter='\t')
+    correct = [x[-2:] for x in correct]
+    print correct
+    return [{"objectName": s, "correctCategory": v} for s, v in correct]
+
+
+def load_input(path):
+    inputt = csv.reader(open(path + "/input"), delimiter='\t')
+    return [{'workerName':wn, 'objectName':on, 'categoryName':cn}
+                                                    for wn, on, cn in inputt]
+
+
+def load_all(path):
+    r = [list(csv.reader(open(path + s), delimiter='\t'))
+                            for s in ['/correct', '/costs', '/input']]
+    r[0] = [x[-2:] for x in r[0]]
+    return r
+
+
+def transform_cost(cost):
+    dictt = {}
+    for c1, c2, cost_ in cost:
+        el = dictt.get(c1, {})
+        el[c2] = cost_
+        dictt[c1] = el
+    return dictt.items()
+
+
+def test_all(dsas, correct, cost, inputt):
+
+    ID = "123"
+    
+    dsas.ping()
+    dsas.reset(ID)
+
+    cost = transform_cost(cost)
+
+    dsas.load_categories(cost, ID)
+    dsas.load_gold_labels(correct, ID)
+    dsas.load_worker_assigned_labels(inputt, ID)
+    dsas.compute(10, ID)
+    time.sleep(3)
+    pprint.pprint(dsas.get_dawid_skene(ID))
+
+
+dsas = TroiaClient("http://localhost:8080/GetAnotherLabel/rest/", None)
+main_path = "examples/"
+
+data = load_all(sys.argv[1])
+
+test_all(dsas, *data)
