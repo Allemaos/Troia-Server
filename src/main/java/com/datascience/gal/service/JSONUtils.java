987d925194e9984554cce72b114d3d14a940c324
Konrad Kurdej <konrad.kurdej@10clouds.com>
2012-10-24 13:34:42 +0200

    Important fix in timing

--- src/main/java/com/datascience/gal/service/JSONUtils.java
+++ src/main/java/com/datascience/gal/service/JSONUtils.java
@@ -129,7 +129,7 @@ public class JSONUtils {
 		String ret = gson.toJson(src);
 		stopwatch.stop();
 		heapSize = ((runtime.totalMemory() - runtime.freeMemory()) - heapSize) / 1024. / 1024.;
-		logger.info("JSONing: " + src.getClass().getSimpleName() + " took: " + (stopwatch.getSplitTime() / 1000.) + 
+		logger.info("JSONing: " + src.getClass().getSimpleName() + " took: " + (stopwatch.getTime() / 1000.) + 
 				"s len: " + ret.length() + " size: " + heapSize);
 		return ret;
 	}
